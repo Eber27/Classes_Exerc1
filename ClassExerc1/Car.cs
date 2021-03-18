9ccb962d6873c5671c3ff14d63b4ccb435fb733f
Eber27 <eberfreire@gmail.com>
2021-03-17 22:03:15 -0500

    Classes_Exerc1

---
+++ ClassExerc1/Car.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassExerc1
+{
+    public class Car
+    {
+        public Car()             // Default Constructor
+        {
+        
+        }
+
+        public Car(string makeinput, string modelinput, int yearinput) // Constructor Assigning Make, Model and Year properties
+        {
+            Make = makeinput;
+            Model = modelinput;
+            Year = yearinput;
+
+        }
+
+
+        //property
+        public string Make { get; set; }
+
+        public string Model { get; set; }
+
+        public int Year { get; set; }
+    }
+}
---
+++ ClassExerc1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ClassExerc1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var Car1 = new Car();
+            Car1.Make = "BMW";
+            Car1.Model = "X5";
+            Car1.Year = 2017;
+
+            // other ways to be done**
+
+            var Car2 = new Car()
+            {
+                Make = "Chevy",
+                Model = "Equinox",
+                Year = 2014
+            };
+            
+            var Car3 = new Car ("Nissan", "Pathfinder", 2016);
+
+            var carList = new List<Car>() { Car1, Car2, Car3 };
+
+            foreach (var vehicle in carList)
+            {
+                Console.WriteLine($"{vehicle.Make}, {vehicle.Model}, {vehicle.Year}");
+            }
+        }
+    }
+}
